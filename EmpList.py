bf828c8e9e8a8d695e0b12528509b7d82f099bd3
cyrushow <caiweihow@gmail.com>
2024-07-17 10:24:34 +0800

    Update EmpList.py
    added and copied same print line

--- EmpList.py
+++ EmpList.py
@@ -3,3 +3,4 @@ from EmpClass import Employee
 emp5 = Employee("Darren", "Tan", "M003")
 #print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName)
 print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName)
+print(emp1.FullName, emp2.FullName, emp3.FullName, emp4.FullName, emp5.FullName)
